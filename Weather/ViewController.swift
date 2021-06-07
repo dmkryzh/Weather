3aee506b6f3350ed03fc05ed70d20eb3aee8b4f8
Dmitrii Kryzhanovskii <dmkryzh@gmail.com>
2021-06-07 21:39:24 +0300

    Weather init

---
+++ Weather/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Dmitrii KRY on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
